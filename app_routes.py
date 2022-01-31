b6b4b313c345221e6c36e9c15cb6bd2aecc1e944
Целиков Владимир Эдуардович <49162341+vladimir2142@users.noreply.github.com>
2022-02-01 00:30:28 +0500

    example: create db

--- app_routes.py
+++ app_routes.py
@@ -1,9 +1,33 @@
 import datetime
 import uvicorn
 import logging
+import sqlite3
 
 from fastapi import FastAPI
+from fastapi.param_functions import Depends
 from pydantic import BaseModel
+from pydantic import BaseSettings
+from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.ext.asyncio import engine
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from data_base.init_db import create_table 
+
+class Settings(BaseSettings):
+    sqlite_config: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
+
+engine = engine.create_async_engine(settings.sqlite_config, echo=True)
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
 
 #Class for post request body
 class Item(BaseModel):
@@ -24,7 +48,12 @@ def get_request(name:str):
 def post_request(item:Item):
     """Example of post request"""
     return item
-    
+
+@app.post('/db_write/')
+async def add_info(session: AsyncSession = Depends(get_session)):
+    await create_table(session)
+
+
 #Starting local server 
 if __name__ == "__main__":
     uvicorn.run("app_routes:app", host="0.0.0.0", port=2142, log_level="info",reload=True)
